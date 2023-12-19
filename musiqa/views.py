7ca3b00a709bf14e198cfda2ea6fe08f6d785e6d
AXurshidbek <abdubannobovx@gmail.com>
2023-12-20 00:04:31 +0500

    vazifa 19/12/23

--- musiqa/views.py
+++ musiqa/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
+from django.contrib.postgres.search import TrigramSimilarity
+from rest_framework.pagination import PageNumberPagination
 from .models import *
 from .serializers import *
 
@@ -11,9 +13,22 @@ from .serializers import *
 class QoshiqModelViewSet(ModelViewSet):
     queryset = Qoshiq.objects.all()
     serializer_class = QoshiqSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
     search_fields = [ 'nom','janr']
     order_fields = ['davomiylik']
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 3
 
+    def get_queryset(self):
+        qoshiqlar = self.queryset
+        janr = self.request.query_params.get('janr')
+        qidiruv = self.request.query_params.get("qidiruv")
+        if qidiruv:
+            qoshiqlar = Qoshiq.objects.annotate(oxshashlik=TrigramSimilarity('nomm', qidiruv)
+                                                ).filter(oxshashlik__gt=0.3).order_by('-oxshashlik')
+        if janr:
+            kinolar = qoshiqlar.filter(janr__contains=janr)
+        return qoshiqlar
     def list(self, request, *args, **kwargs):
         qoshiqlar=self.queryset
         serializer=QoshiqSerializer(qoshiqlar, many=True)
